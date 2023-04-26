6150fe1796a121e84456f69a8bbd53e032f2c0ac
Nick Orsolits <n.orsolits@gmail.com>
2023-04-26 22:28:35 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp25/CaesarCipher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    public static class CaesarCipher
+    {
+        static string text = "Lfk elq ghu Vlprq ghu jhuqh Edqdqhq lvvw xqg plw vhlqhp Kxqg vslhow ";
+
+        private const int Offset = 32;
+
+
+        public static int Encode(char letter)
+        {
+            letter = char.ToLower(letter);
+            return (int)letter - Offset;
+        }
+        public static char Decode(int code)
+        {
+            return (char)(code + Offset);
+        }
+
+        public static void chtext()
+        {
+            for (int i = 1; i <= 26; i++)
+            {
+                Console.Write(i + ": ");
+                for (int k = 0; k < text.Length; k++)
+                {
+                    if (text[k] == ' ') Console.Write(" ");
+                    else
+                    {
+                        int write = Encode(text[k]);
+                        for (int l = 0; l< i; l++)
+                        {
+                            write++;
+                            if (write > 90) write = 65;
+                        }
+
+                        Console.Write(Decode(write));
+                    }
+
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    
+    }
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CaesarCipher.chtext();
+            Console.WriteLine(CaesarCipher.Encode('a'));
+        }
+    }
+}
